33081e4fa23c946a6e350a2564cfd8ba9827ac29
yang <819836692@qq.com>
2020-01-26 17:25:28 +0800

    第一次修改提交

---
+++ src/components/v-header/v-header.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="v-header">
+
+  </div>
+</template>
+
+<script>
+  import SupportIco from 'components/support-ico/support-ico.vue'
+
+  export default {
+    name: 'v-header',
+    props: {
+      seller: {
+        tyle: Object,
+        default () {
+          return {}
+        }
+      }
+    },
+    components: {
+      SupportIco
+    }
+  }
+</script>
+
+<style lang="stylus" scoped>
+  @import "~common/stylus/mixin"
+  @import "~common/stylus/variable"
+
+</style>
